ea99c20eb4b846fe7082bcafd1955b53543e3452
Barron Henderson <barronh@gmail.com>
2013-12-22 14:55:22 -0500

    Adding better support for multiple mechanisms

--- src/pygeos2cmaq/__main__.py
+++ src/pygeos2cmaq/__main__.py
@@ -1,22 +1,33 @@
 from optparse import OptionParser
 from glob import glob
 import os
+from warnings import warn
 
 def run():
+    defaultmech = "%s/mapping/cb05cl_ae6_aq.csv" % os.path.dirname(__file__)
     parser = OptionParser()
     parser.set_usage("Usage: %prog [-tq] \n"+(" "*16)+" [-i <init name>] [-f <final name>] <yamlfile>")
     parser.add_option("-t", "--template", dest = "template", action = "store_true", default = False, help="Output template on standard out (configurable with -m and -c", metavar="Template")
 
     parser.add_option("-v", "--verbose", dest = "verbose", action = "count", default = 0, help = "extra output for debugging", metavar = "VERBOSE")
     
-    paths = glob(os.path.join(os.path.dirname(__file__), 'mapping', '*_*.yaml'))
-    mechanisms = ', '.join(['_'.join(path.split('/')[-1].split('_')[1:])[:-5] for path in paths])
-    parser.add_option("-c", "--configuration", dest="configuration", default="%s/mapping/cb05cl_ae6_aq.csv" % os.path.dirname(__file__),
-                        help="Chemical mechanisms: %s (for use with -t)" % mechanisms, metavar="CONFIG")
+    paths = glob(os.path.join(os.path.dirname(__file__), 'mapping', '*_*.csv'))
+    mechanisms = ', '.join(['_'.join(path.split('/')[-1].split('_')[:])[:-4] for path in paths])
+    parser.add_option("-c", "--configuration", dest="configuration", default = None,
+                        help = "Chemical mechanisms: %s (for use with -t)" % mechanisms, metavar="CONFIG")
 
     (options, args) = parser.parse_args()
     if options.template:
         from template import template
+        if options.configuration is None:
+            warn("Using default mechanism: %s" % defaultmech)
+        else:
+            if os.path.exists(options.configuration):
+                pass
+            else:
+                options.configuration = "%s/mapping/%s.csv" % (os.path.dirname(__file__), options.configuration)
+                if not os.path.exists(options.configuration):
+                    raise IOError('Cannot find file %s; must be either you own file or in %s' % (options.configuration, mechanisms))
         print template(options.configuration)
         parser.exit()
     if len(args)<1:
