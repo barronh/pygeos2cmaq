3f7be705ec79af167db813a05e975d1cfa38ded9
Barron Henderson <barronh@gmail.com>
2013-12-22 14:44:34 -0500

    Making mode more transparent and adding errors to file log.

--- src/pygeos2cmaq/repair_ae.py
+++ src/pygeos2cmaq/repair_ae.py
@@ -49,72 +49,75 @@ class speciesstruct(object):
         version - 0: both, 5: AERO5, 6: AERO6
         """
         self.name = name
-        self.mode = {1: 'ATKN', 2: 'ACC', 3: 'COR'}[mode]
+        self.mode = mode
         self.density = density
         self.version = version
         self.found = found
 
 def getbcspecies():         
-    bcspecies = [speciesstruct (    'ACLI',   0,    1, RHOSEAS, 0, False),
-                 speciesstruct (    'AECI',   0,    1, RHOANTH, 0, False),
-                 speciesstruct (    'ANAI',   0,    1, RHOSEAS, 0, False),
-                 speciesstruct (   'ANH4I',   0,    1,  RHONH4, 0, False),
-                 speciesstruct (   'ANO3I',   0,    1,  RHONO3, 0, False),
-                 speciesstruct (   'ASO4I',   0,    1,  RHOSO4, 0, False),
-                 speciesstruct (   'AALKJ',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ABNZ1J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ABNZ2J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ABNZ3J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (    'ACLJ',   0,    2, RHOSEAS, 0, False),
-                 speciesstruct (    'AECJ',   0,    2, RHOANTH, 0, False),
-                 speciesstruct (  'AISO1J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AISO2J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AISO3J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (    'ANAJ',   0,    2, RHOSEAS, 0, False),
-                 speciesstruct (   'ANH4J',   0,    2,  RHONH4, 0, False),
-                 speciesstruct (   'ANO3J',   0,    2,  RHONO3, 0, False),
-                 speciesstruct (  'AOLGAJ',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AOLGBJ',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AORGCJ',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (   'ASO4J',   0,    2,  RHOSO4, 0, False),
-                 speciesstruct (   'ASQTJ',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ATOL1J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ATOL2J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ATOL3J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ATRP1J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'ATRP2J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AXYL1J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AXYL2J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (  'AXYL3J',   0,    2,  RHOORG, 0, False),
-                 speciesstruct (    'ACLK',   0,    3, RHOSEAS, 0, False),
-                 speciesstruct (   'ACORS',   0,    3, RHOANTH, 0, False),
-                 speciesstruct (   'ANH4K',   0,    3,  RHONH4, 0, False),
-                 speciesstruct (   'ANO3K',   0,    3,  RHONO3, 0, False),
-                 speciesstruct (   'ASO4K',   0,    3,  RHOSO4, 0, False),
-                 speciesstruct (   'ASOIL',   0,    3, RHOSOIL, 0, False),
-                 speciesstruct (    'A25I',   0,    1, RHOANTH, 5, False),
-                 speciesstruct (    'A25J',   0,    2, RHOANTH, 5, False),
-                 speciesstruct (    'ANAK',   0,    3, RHOSEAS, 5, False),
-                 speciesstruct ( 'AORGPAI',   0,    1,  RHOORG, 5, False),
-                 speciesstruct ( 'AORGPAJ',   0,    2,  RHOORG, 5, False),
-                 speciesstruct (    'AALJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct (    'ACAJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct (    'AFEJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct (     'AKJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct (    'AMGJ',   0,    2, RHOSEAS, 6, False),
-                 speciesstruct (    'AMNJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct (  'AOTHRI',   0,    1, RHOANTH, 6, False),
-                 speciesstruct (  'AOTHRJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct ( 'APNCOMI',   0,    1,  RHOORG, 6, False),
-                 speciesstruct ( 'APNCOMJ',   0,    2,  RHOORG, 6, False),
-                 speciesstruct (   'APOCI',   0,    1,  RHOORG, 6, False),
-                 speciesstruct (   'APOCJ',   0,    2,  RHOORG, 6, False),
-                 speciesstruct ( 'ASEACAT',   0,    3, RHOSEAS, 6, False),
-                 speciesstruct (    'ASIJ',   0,    2, RHOSOIL, 6, False),
-                 speciesstruct (    'ATIJ',   0,    2, RHOSOIL, 6, False)]
+    bcspecies = [speciesstruct (    'ACLI',   0, 'ATKN', RHOSEAS, 0, False),
+                 speciesstruct (    'AECI',   0, 'ATKN', RHOANTH, 0, False),
+                 speciesstruct (    'ANAI',   0, 'ATKN', RHOSEAS, 0, False),
+                 speciesstruct (   'ANH4I',   0, 'ATKN',  RHONH4, 0, False),
+                 speciesstruct (   'ANO3I',   0, 'ATKN',  RHONO3, 0, False),
+                 speciesstruct (   'ASO4I',   0, 'ATKN',  RHOSO4, 0, False),
+                 speciesstruct (    'A25I',   0, 'ATKN', RHOANTH, 5, False),
+                 speciesstruct ( 'AORGPAI',   0, 'ATKN',  RHOORG, 5, False),
+                 speciesstruct (  'AOTHRI',   0, 'ATKN', RHOANTH, 6, False),
+                 speciesstruct ( 'APNCOMI',   0, 'ATKN',  RHOORG, 6, False),
+                 speciesstruct (   'APOCI',   0, 'ATKN',  RHOORG, 6, False),
+                 speciesstruct (   'AALKJ',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ABNZ1J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ABNZ2J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ABNZ3J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (    'ACLJ',   0,  'ACC', RHOSEAS, 0, False),
+                 speciesstruct (    'AECJ',   0,  'ACC', RHOANTH, 0, False),
+                 speciesstruct (  'AISO1J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AISO2J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AISO3J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (    'ANAJ',   0,  'ACC', RHOSEAS, 0, False),
+                 speciesstruct (   'ANH4J',   0,  'ACC',  RHONH4, 0, False),
+                 speciesstruct (   'ANO3J',   0,  'ACC',  RHONO3, 0, False),
+                 speciesstruct (  'AOLGAJ',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AOLGBJ',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AORGCJ',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (   'ASO4J',   0,  'ACC',  RHOSO4, 0, False),
+                 speciesstruct (   'ASQTJ',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ATOL1J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ATOL2J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ATOL3J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ATRP1J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'ATRP2J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AXYL1J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AXYL2J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (  'AXYL3J',   0,  'ACC',  RHOORG, 0, False),
+                 speciesstruct (    'A25J',   0,  'ACC', RHOANTH, 5, False),
+                 speciesstruct ( 'AORGPAJ',   0,  'ACC',  RHOORG, 5, False),
+                 speciesstruct (    'AALJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (    'ACAJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (    'AFEJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (     'AKJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (    'AMGJ',   0,  'ACC', RHOSEAS, 6, False),
+                 speciesstruct (    'AMNJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (  'AOTHRJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct ( 'APNCOMJ',   0,  'ACC',  RHOORG, 6, False),
+                 speciesstruct (   'APOCJ',   0,  'ACC',  RHOORG, 6, False),
+                 speciesstruct (    'ASIJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (    'ATIJ',   0,  'ACC', RHOSOIL, 6, False),
+                 speciesstruct (    'ACLK',   0,  'COR', RHOSEAS, 0, False),
+                 speciesstruct (   'ACORS',   0,  'COR', RHOANTH, 0, False),
+                 speciesstruct (   'ANH4K',   0,  'COR',  RHONH4, 0, False),
+                 speciesstruct (   'ANO3K',   0,  'COR',  RHONO3, 0, False),
+                 speciesstruct (   'ASO4K',   0,  'COR',  RHOSO4, 0, False),
+                 speciesstruct (   'ASOIL',   0,  'COR', RHOSOIL, 0, False),
+                 speciesstruct (    'ANAK',   0,  'COR', RHOSEAS, 5, False),
+                 speciesstruct ( 'ASEACAT',   0,  'COR', RHOSEAS, 6, False)]
     return bcspecies
 
-def repair_ae(f):
+def repair_ae(f, myioo):
+    warn = myioo.warn
+    status = myioo.status
+    error = myioo.error
     bcspcs = getbcspecies()
     for spc in bcspcs:
         try:
@@ -142,7 +145,7 @@ def repair_ae(f):
     if len(version_check) > 1:
         warn('Some variables from aerosol versions %s were not found' % ' and '.join([str(v) for v in version_check]))
     moment3 = dict([(k, zeros((ntimes, nlayers, nperim), dtype = 'f')) for k in 'ATKN ACC COR'.split()])
-
+    
     for spc in bcspcs:
         try:
             bcval = f.variables[spc.name]
